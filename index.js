b8c00b2e41227dda09079a5029dc8a806a5319fe
martinjnolmedo <olmedo.molmedo@gmail.com>
2025-06-05 01:37:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const BASE_URL = 'https://fakestoreapi.com';
 
-if (argv[2].toLocaleLowerCase() == "GET".toLocaleLowerCase()) {
-    console.log("Toma un dato");
-} else if (argv[2].toLocaleLowerCase() == "POST".toLocaleLowerCase()) {
-    console.log(`Recibimos ${argv[3]} satisfactoriamente.`);
-} else if (argv[2].toLocaleLowerCase() == "PUT".toLocaleLowerCase()) {
-    console.log(`Modificamos el item con id: ${argv[3]} satisfactoriamente.`);
-} else if (argv[2].toLocaleLowerCase() == "DELETE".toLocaleLowerCase()) {
-    console.log(`El item con el id: ${argv[3]} se eliminó con éxito.`);
+fetch('https://fakestoreapi.com/products/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+async function main() {
+    if (argv.length > 1) {
+        if (argv[2] == "GET") {
+            console.log("Toma un dato");
+        } else if (argv[2] == "POST") {
+            console.log(`Recibimos ${argv[3]} satisfactoriamente.`);
+        } else if (argv[2] == "PUT") {
+            console.log(`Modificamos el item con id: ${argv[3]} satisfactoriamente.`);
+        } else if (argv[2] == "DELETE") {
+            console.log(`El item con el id: ${argv[3]} se eliminó con éxito.`);
+        }
+    } else {
+        console.log("Error.")
+    }
 }
+
+main();
